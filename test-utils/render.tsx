1f2a20823a93db646bef801615bc5de733a400be
Roman Rizvanov <progbeat@gmail.com>
2024-11-01 09:15:19 +0100

    Update .gitignore to include /dist and simplify render function in test-utils

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,11 +1,7 @@
 import { render as testingLibraryRender } from '@testing-library/react';
-import { MantineProvider } from '@mantine/core';
-import { theme } from '../src/theme';
 
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(ui, {
-    wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={theme}>{children}</MantineProvider>
-    ),
+    wrapper: ({ children }: { children: React.ReactNode }) => children,
   });
 }
